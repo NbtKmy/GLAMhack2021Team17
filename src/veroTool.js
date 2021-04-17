f43025a95cc8e21bc2fb509b5479bfd94f5c7556
NbtKmy <nobuta77to99@hotmail.com>
2021-04-17 16:27:20 +0200

    change

--- src/veroTool.js
+++ src/veroTool.js
@@ -1,13 +1,40 @@
 var vrvToolkit = new verovio.toolkit();
+var zoom = 50;
+var pageHeight = 800;
+var pageWidth = 800;
 
-            ////////////////////////////////////
-            /* Load the file using a HTTP GET */
-            ////////////////////////////////////
-            $.ajax({
-                url: "./mei/default.mei"
-                , dataType: "text"
-                , success: function(data) {
-                    var svg = vrvToolkit.renderData(data, {});
-                    $("#svg_output").html(svg);
-                }
-            });
+function setOptions() {
+    //////////////////////////////////////////////////////////////
+    /* Adjust the height and width according to the window size */
+    //////////////////////////////////////////////////////////////
+    pageHeight = $(document).height() * 100 / zoom ;
+    pageWidth = $(window).width() * 100 / zoom ;
+    options = {
+                pageHeight: pageHeight,
+                pageWidth: pageWidth,
+                scale: zoom,
+                adjustPageHeight: true
+            };
+    vrvToolkit.setOptions(options);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+/* A function that sets the options, loads the data and render the first page */
+////////////////////////////////////////////////////////////////////////////////
+function loadData(data) {
+    setOptions();
+    vrvToolkit.loadData(data);
+    svg = vrvToolkit.renderToSVG(1, {});
+    $("#svg_output").html(svg);
+}
+
+$(document).ready(function() {
+    var file = ":/mei/default.mei";
+    $.ajax({
+        url: file
+        , dataType: "text"
+        , success: function(data) {
+            loadData(data);
+        }
+    });
+});
