212a00534972a6a9ad78a252bf6120b533d0f756
NbtKmy <nobuta77to99@hotmail.com>
2021-04-19 23:08:54 +0200

    change

--- src/veroTool.js
+++ src/veroTool.js
@@ -1,5 +1,9 @@
-var vrvToolkit = new verovio.toolkit();
+const vrvToolkit = new verovio.toolkit();
+const picoAudio = new PicoAudio();
+
 var zoom = 30;
+var ids = [];
+var isPlaying = false;
 
 function setOptions() {
     let pageHeight = $("#svg_output").height() * 100 / zoom ;
@@ -13,16 +17,32 @@ function setOptions() {
     vrvToolkit.setOptions(options);
 }
 
-
 function loadData(data) {
     setOptions();
     vrvToolkit.loadData(data);
-    svg = vrvToolkit.renderToSVG(1, {});
-    $("#svg_output").html(svg);
+
+    page = 1;
+    loadPage();
 }
 
-$(document).ready(function() {
-    var file = "./mei/default.mei";
+function loadPage() {
+    svg = vrvToolkit.renderToSVG(page, {});
+    $("#svg_output").html(svg);
+
+    ////////////////////////////////////////
+    /* Bind a on click event to each note */
+    ////////////////////////////////////////
+    $(".note").click(function() {
+        var id = $(this).attr("id");
+        var time = vrvToolkit.getTimeForElement(id);
+        //$("#midi-player").midiPlayer.seek(time);
+    });
+};
+
+
+
+function loadFile() {
+    file = "./mei/default.mei";
     $.ajax({
         url: file
         , dataType: "text"
@@ -30,4 +50,36 @@ $(document).ready(function() {
             loadData(data);
         }
     });
+}
+
+
+// Midi Player
+function play_midi() {
+    if (isPlaying == false) {
+        picoAudio.init();
+        picoAudio.play();
+        isPlaying = true;
+    };
+}
+
+// Midi stopper
+function stop_midi() {
+        ids.forEach(function(noteid) {
+            $("#" + noteid).attr("fill", "#000").attr("stroke", "#000");
+        });
+        picoAudio.init();
+        picoAudio.pause();
+        isPlaying = false;
+}
+
+$(document).ready(function() {
+
+    loadFile();
+    // set the Midi data
+    var base64midi = vrvToolkit.renderToMIDI();
+    var song = 'data:audio/midi;base64,' + base64midi;
+    const smfData = new Uint8Array(song);
+    const parsedData = picoAudio.parseSMF(smfData);
+    picoAudio.setData(parsedData);
+
 });
